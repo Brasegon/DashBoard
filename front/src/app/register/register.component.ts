bcec91c1096b141bf7f857886dcf3e2ab5e6cbfa
Valounte <valentin.lyon@epitech.eu>
2020-12-09 11:34:03 +0100

    Register secure

--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -5,6 +5,8 @@ import { TokenService } from '../api/token.service';
 import { SocialAuthService } from "angularx-social-login";
 import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
 import { SocialUser } from "angularx-social-login";
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -14,7 +16,7 @@ export class RegisterComponent implements OnInit {
 
   user: SocialUser | undefined;
   loggedIn: boolean | undefined;
-  
+
   constructor(private auth : AuthService, private token: TokenService, private authService: SocialAuthService) { }
 
 
@@ -38,15 +40,108 @@ export class RegisterComponent implements OnInit {
 
   async registerGoogle(user : any) {
       var result = await this.auth.registerGoogle({email: user.email, Oauth: "GOOGLE"}).toPromise();
+      console.log(result);
+      if (result.status === "error") {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Username or Email already exits',
+          confirmButtonColor : '#3DB1C8',
+          confirmButtonText: 'OK !'
+        });
+      }
+      else {
+        this.success_message();
+      }
   }
 
   // tslint:disable-next-line:typedef
-  btn_register() {
-    this.auth.register({login: this.registerConnection.value.username, email: this.registerConnection.value.email, password: this.registerConnection.value.password1}).toPromise()
+  IsEmailValid(email) {
+    return /\S+@\S+\.\S+/.test(email);
   }
+  // tslint:disable-next-line:typedef
+ async btn_register(){
+      const login = this.registerConnection.value.username;
+      const email = this.registerConnection.value.email;
+      const password = this.registerConnection.value.password1;
+
+      if (this.IsEmailValid(email) !== true) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Your email is not valid...',
+          confirmButtonColor : '#3DB1C8',
+          confirmButtonText: 'OK !'
+        });
+        return false;
+      }
+      if (this.IsUsernameValid(login) !== true) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Username must have 5-15 characters and be composed only of numbers and alphabets',
+            confirmButtonColor : '#3DB1C8',
+            confirmButtonText: 'OK !'
+          });
+          return false;
+      }
+
+      if (password.length < 8) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Your password must have at least 8 characters',
+          confirmButtonColor : '#3DB1C8',
+          confirmButtonText: 'OK !'
+        });
+        return false;
+      }
+      var result = await this.auth.register({login: this.registerConnection.value.username, email: this.registerConnection.value.email, password: this.registerConnection.value.password1}).toPromise()
+      if (result.status === "error") {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Username or Email already exits',
+          confirmButtonColor : '#3DB1C8',
+          confirmButtonText: 'OK !'
+        });
+      }
+      else {
+        this.success_message();
+      }
+
+    }
 
   signInWithGoogle(): void {
     let isCalled = false;
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
+
+  // tslint:disable-next-line:typedef
+  IsUsernameValid(str: any) {
+    const illegalChars = /\W/;
+
+    if (str === '') {
+      return false;
+    } else if ((str.length < 5) || (str.length > 15)) {
+        return false;
+    } else if (illegalChars.test(str)) {
+        return false;
+    }
+    return true;
+  }
+
+  success_message() {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'You have been registered',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    setTimeout(function() {
+     window.location.href = "/login";
+  }, 1500);
+  }
+
 }
